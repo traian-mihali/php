99af3a12c33d9823b04300de16e16e19ac81719f
Traian <traianmihali696@gmail.com>
2019-08-23 14:54:52 +0300

    add the styles into separate a file

--- mvc/index.php
+++ mvc/index.php
@@ -1,29 +1,7 @@
 <html>
   <head>
-    <style>
-      body {
-        padding: 30px;
-      }
-
-      h1 {
-        color: rgb(64, 64, 192);
-      }
-
-      button {
-        background-color: rgb(95, 95, 214);
-        padding: 10px 16px;
-      }
-
-      button > a {
-        text-decoration-line: none;
-        color: white;
-        font-size: 1rem;
-      }
-
-      button:hover {
-        background-color: rgb(92, 92, 126);
-      }
-    </style>
+    <link rel="stylesheet" href="styles.css" />
+    <title>MVC Pattern</title>
   </head>
 
   <body>
